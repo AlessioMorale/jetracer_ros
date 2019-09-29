531d4550a8cc19a4b8b98807dd76349ea80985e4
alessio <jetracer@jetracer.local>
2019-09-29 01:18:48 +0100

    cleanup trainer node

--- scripts/trainer_node.py
+++ scripts/trainer_node.py
@@ -26,22 +26,19 @@ class trainer:
     image_counter = 0
     data = []
     last_sample = time()
-    sample_interval_ms = 2000
-    vel_treshold = 0.1
+    sample_interval_ms = 125
+    throttle_threshold = 0.05
 
     def __init__(self):
         self.bridge = CvBridge()
         pass
 
     def save_image(self, image, filename):
-        print("Received an image!")
         try:
-            # Convert your ROS Image message to OpenCV2
             cv2_img = self.bridge.imgmsg_to_cv2(image, "bgr8")
         except CvBridgeError as e:
             print(e)
         else:
-            # Save your OpenCV2 image as a jpeg 
             cv2.imwrite(filename, cv2_img)
 
     def setup_location(self):
@@ -52,34 +49,37 @@ class trainer:
             os.makedirs(self.current_folder)
         filename = os.path.join(self.current_folder, self.csv_filename)
         self.csv_file = open(filename, 'w')
-        rospy.loginfo("Writing %s", filename)
-
+        rospy.loginfo("Capturing into %s", self.current_folder)
 
     def set_storage(self, storage):
         self.storage_location = storage
         rospy.loginfo("storage location %s", self.storage_location)
 
+    def set_sample_interval(self, interval):
+        self.sample_interval_ms = interval
+
+    def set_throttle_threshold(self, threshold):
+        self.throttle_threshold = threshold
+
     def on_camera(self, image):
-        if self.is_recording == False or (abs(self.throttle) < self.vel_treshold):
+        if self.is_recording == False or (abs(self.throttle) < self.throttle_threshold):
             return
         now = time()
         if( (now - self.last_sample) * 1000.0 < self.sample_interval_ms):
             return
-        rospy.loginfo("is_recoridng %s, throttle %d", self.is_recording, self.throttle)
 
         self.last_sample = now
-        #image_name = "".join([uuid.uuid4().hex,'.jpg'])
         image_name = "".join(["{:05d}".format(self.image_counter),".jpg"])
         self.image_counter += 1
         full_path_name = os.path.join(self.current_folder, image_name)
         self.save_image(image, full_path_name)
         self.data.append([image_name,self.steering, self.throttle])
+        rospy.logdebug("Saving {0}, throttle {1:0.2g}, steering: {2:0.2g}".format(image_name, self.throttle,self.steering))
         self.throttle = 0 # wait to receive a Twist msg with throttle != 0 to save the next sample
         
     def on_twist(self, data):
         self.steering = data.angular.z
         self.throttle = data.linear.x
-        rospy.loginfo("steering %s, throttle %s",self.steering, self.throttle)
     
     def on_shutdown(self):
         self.stop_recording()
@@ -90,16 +90,17 @@ class trainer:
         else:
             self.stop_recording()
         return recordingResponse()
+
     def stop_recording(self):
         if not self.is_recording:
             return
+        rospy.loginfo("STOP recording")
         with self.csv_file:
             writer = csv.writer(self.csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             writer.writerow(['image','steering', 'throttle'])
             writer.writerows(self.data)
             self.data = []
         self.is_recording = False
-        rospy.loginfo("stop recording")
     
     def start_recording(self):
         if(self.is_recording):
@@ -107,11 +108,18 @@ class trainer:
         self.throttle = 0 # Wait for throttle to be != to acquire a sample        
         self.setup_location()
         self.is_recording = True
-        rospy.loginfo("start recording")
+        rospy.loginfo("Start recording")
 
 def run(trainer_node):
     camera_topic = rospy.get_param("/trainer/camera_topic")
     manual_twist_topic = rospy.get_param("/trainer/manual_twist_topic")
+
+    sample_interval_ms = rospy.get_param("/trainer/sample_interval")
+    trainer_node.set_sample_interval(sample_interval_ms)
+    
+    throttle_threshold = rospy.get_param("/trainer/throttle_threshold")
+    trainer_node.set_throttle_threshold(throttle_threshold)
+
     storage = rospy.get_param("/trainer/storage")
     trainer_node.set_storage(storage)
     if not os.path.isdir(storage):
